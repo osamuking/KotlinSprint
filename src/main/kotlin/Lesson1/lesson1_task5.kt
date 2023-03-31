bb9a1b997ce34658a6ee46185d5412b8cc5283ad
osamuking <regpurp@outlook.com>
2023-04-01 07:02:23 +0900

    KS1-5 Tried to make an universal code (NG)

--- src/main/kotlin/Lesson1/lesson1_task5.kt
+++ src/main/kotlin/Lesson1/lesson1_task5.kt
@@ -1,8 +1,12 @@
 package Lesson1
 
 fun main() {
-    val flightSeconds = 6480
-    val flightMinutes = flightSeconds/60
+    var flightSeconds = 64763.00f
+    var flightMinutes = flightSeconds/60
 
-    println("$flightMinutes:00")
+    var min = flightMinutes.toInt()
+    var sec = (60 * (flightMinutes - min)).toInt()
+
+    print("$min:$sec")
+
 }
