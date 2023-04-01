8ca062d7d857b78ce055c88e65f7e94bb9568b33
osamuking <regpurp@outlook.com>
2023-04-02 07:34:34 +0900

    Fixed code in lesson1_task5

--- src/main/kotlin/Lesson1/lesson1_task5.kt
+++ src/main/kotlin/Lesson1/lesson1_task5.kt
@@ -4,7 +4,7 @@ fun main() {
     var flightSeconds = 6480
     var flightMinutes = flightSeconds / 60
 
-    var sec = flightSeconds%60
+    var sec = flightSeconds % 60
 
     print("$flightMinutes:$sec")
 
