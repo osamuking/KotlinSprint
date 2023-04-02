87a8b8a053eab208490a0d8936b31a1e2c9c90e8
osamuking <regpurp@outlook.com>
2023-04-02 21:17:11 +0900

    Completed task1 from lesson2

--- src/main/kotlin/Lesson2/lesson2_task1.kt
+++ src/main/kotlin/Lesson2/lesson2_task1.kt
@@ -1,2 +1,12 @@
 package Lesson2
 
+fun main() {
+    val markAthos = 3.0f
+    val markPorthos = 4.0f
+    val markAramis = 3.0f
+    val markDArtangan = 5.0f
+
+    val averageMark = (markAthos + markPorthos + markAramis + markDArtangan) / 4
+
+    print("Средний балл составляет " + averageMark)
+}
