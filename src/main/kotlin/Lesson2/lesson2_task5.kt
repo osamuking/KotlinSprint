94ec0a43335aafe50e023275df5222395acb7a26
osamuking <regpurp@outlook.com>
2023-04-02 21:12:17 +0900

    Done all tasks of lesson2

--- src/main/kotlin/Lesson2/lesson2_task5.kt
+++ src/main/kotlin/Lesson2/lesson2_task5.kt
@@ -1,16 +1,17 @@
 package Lesson2
 
-fun main(){
-    // x = s * (1 + p/100)^n
-    /* fun exp(a: Double, b: Double): Double {
-       return Math.exp(Math.log(a) * b) } */
-    // String.format("%.3f", x)
+fun main() {
+
     val deposit = 70_000.0
     val interestRate = 16.7
-    val years = 20
+    val years = 20.0
     var sum: Double
+    var s: Double
 
-
-
-    println("Сумма через 20 лет составит " + sum + " рублей.")
+    s = (1 + interestRate / 100)
+    sum = deposit * pow(s, years)
+    println("Сумма вклада через 20 лет составит " + String.format("%.3f", sum) + " рублей.")
+}
+fun pow(a: Double, b: Double): Double {
+    return Math.exp(Math.log(a) * b)
 }
