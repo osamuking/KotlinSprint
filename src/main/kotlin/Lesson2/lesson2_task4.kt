fa6bd68b0b38134300f8ba3b73b32431895f8df2
osamuking <regpurp@outlook.com>
2023-04-03 22:13:37 +0900

    Completed task4 from lesson2

--- src/main/kotlin/Lesson2/lesson2_task4.kt
+++ src/main/kotlin/Lesson2/lesson2_task4.kt
@@ -1,2 +1,14 @@
 package Lesson2
 
+fun main(){
+
+    val crystalOre = 7
+    val ironOre = 11
+
+    val crystalBuff =  crystalOre*20/100
+    val ironBuff = ironOre*20/100
+
+    println("\"Бонус\"для кристаллической руды: $crystalBuff")
+    println("\"Бонус\"для железной руды: $ironBuff")
+    
+}
