c515967a7a5db34faf927ff1ad257db003ea6e99
osamuking <regpurp@outlook.com>
2023-04-03 15:37:13 +0900

    Completed task1 from lesson2

---
+++ src/main/kotlin/Lesson2/lesson2_task1.kt
@@ -0,0 +1,13 @@
+package Lesson2
+
+fun main(){
+
+    val markAthos = 3.0f
+    val markPorthos = 4.0f
+    val markAramis = 3.0f
+    val markDArtangan = 5.0f
+
+    val averageMark = (markAthos + markPorthos + markAramis + markDArtangan) / 4
+
+    println("Средний балл составляет " + averageMark)
+}
