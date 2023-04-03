a8ef1926cc530bfc6ded1b1062762e880cf289e3
osamuking <regpurp@outlook.com>
2023-04-03 22:05:41 +0900

    Completed task3 from lesson2

--- src/main/kotlin/Lesson2/lesson2_task3.kt
+++ src/main/kotlin/Lesson2/lesson2_task3.kt
@@ -1,2 +1,22 @@
 package Lesson2
 
+fun main() {
+
+    val departureHour = 9
+    val departureMinute = 39
+    val travelTime = 450 //minutes
+
+    val travelHours = (travelTime / 60)
+    val travelMinutes = travelTime % 60
+    var arrivingHour = departureHour + travelHours
+    var arrivingMinute = departureMinute + travelMinutes
+
+    if (arrivingMinute > 60) {
+        arrivingHour += 1
+        arrivingMinute -= 60
+    }
+
+    println("Arriving time: ${"%02d".format(arrivingHour)}:" +
+            "${"%02d".format(arrivingMinute)}" )
+
+}
