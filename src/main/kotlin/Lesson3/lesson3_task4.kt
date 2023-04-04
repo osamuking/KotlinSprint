13fe5e7165dce914a59c8ee30a8d08e1ec9bb4fb
osamuking <regpurp@outlook.com>
2023-04-04 10:50:06 +0900

    Completed task4 from lesson3

--- src/main/kotlin/Lesson3/lesson3_task4.kt
+++ src/main/kotlin/Lesson3/lesson3_task4.kt
@@ -1,2 +1,26 @@
 package Lesson3
 
+fun main() {
+
+    var moveFrom = "E2"
+    var moveTo = "E4"
+    var moveNumber = 1
+
+    var currentState = "$moveFrom-$moveTo; $moveNumber"
+
+    println(currentState)
+
+    moveFrom = "E4"
+    moveTo = "F5"
+    moveNumber = 2
+
+    println("$moveFrom-$moveTo; $moveNumber")
+
+
+    moveFrom = "D2"
+    moveTo = "D3"
+    moveNumber = 3
+
+    println("$moveFrom-$moveTo; $moveNumber")
+
+}
