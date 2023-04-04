71f250fa566b7c3c5e06398f9c8878bc98da478d
osamuking <regpurp@outlook.com>
2023-04-04 09:14:52 +0900

    Completed task1 from lesson3

--- src/main/kotlin/Lesson5/lesson5_task5.kt
+++ src/main/kotlin/Lesson5/lesson5_task5.kt
@@ -1,2 +1,14 @@
 package Lesson5
 
+fun main(){
+
+    var greeting = "Good morning"
+    var userName = "dear customer"
+
+    println("$greeting, $userName!")
+
+    greeting = "Good evening"
+    userName = "valuable customer"
+
+    println("$greeting, $userName!")
+}
