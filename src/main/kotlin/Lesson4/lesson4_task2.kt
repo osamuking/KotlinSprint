430fa97cc7123824af74edf42ebe130b7c79bf84
osamuking <regpurp@outlook.com>
2023-04-04 15:25:32 +0900

    Completed task2 from lesson4

--- src/main/kotlin/Lesson4/lesson4_task2.kt
+++ src/main/kotlin/Lesson4/lesson4_task2.kt
@@ -1,2 +1,31 @@
 package Lesson4
 
+fun main() {
+
+    var weight = 50
+    var volume = 100
+
+    println("Груз весом $weight кг и объемом $volume л относится" +
+            " к категории Average: ${(weight >= MIN_WEIGHT && weight
+                                     <= MAX_WEIGHT) && volume < VOLUME}")
+
+    weight = 20
+    volume = 80
+
+    println("Груз весом $weight кг и объемом $volume л относится" +
+                " к категории Average: ${(weight >= MIN_WEIGHT && weight
+                                          <= MAX_WEIGHT) && volume < VOLUME}")
+
+    weight = 50
+    volume = 100
+
+    println(
+        "Груз весом $weight кг и объемом $volume л относится" +
+                " к категории Average: ${(weight >= MIN_WEIGHT && weight
+                                          <= MAX_WEIGHT) && volume < VOLUME}")
+
+}
+
+const val MAX_WEIGHT = 100
+const val MIN_WEIGHT = 35
+const val VOLUME = 100
