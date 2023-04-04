3af6ecb975fb90635c47b0395ef1c4327e0d54bc
osamuking <regpurp@outlook.com>
2023-04-04 11:42:47 +0900

    Completed task5 from lesson3

--- src/main/kotlin/Lesson3/lesson3_task5.kt
+++ src/main/kotlin/Lesson3/lesson3_task5.kt
@@ -1,2 +1,16 @@
 package Lesson3
 
+fun main(){
+
+    val currentState = "D2-D4;0"
+
+    val stateInfo = currentState.split("-", ";")
+
+    val moveFrom = stateInfo[0]
+    val moveTo = stateInfo[1]
+    val moveNumber = stateInfo[2]
+
+    println("Move from: $moveFrom")
+    println("Move to: $moveTo")
+    println("Move number: $moveNumber")
+}
