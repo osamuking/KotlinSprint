9717fe5769f122c09e82663266da48163532bb9e
osamuking <regpurp@outlook.com>
2023-04-04 13:15:18 +0900

    Completed task1 from lesson4

--- src/main/kotlin/Lesson4/lesson4_task1.kt
+++ src/main/kotlin/Lesson4/lesson4_task1.kt
@@ -1,2 +1,14 @@
 package Lesson4
 
+fun main(){
+
+    var orderedToday = 13
+    var orderedTomorrow = 9
+
+    val isAvailableToday = orderedToday < TABLES_NUMBER
+    val isAvailableTomorrow = orderedTomorrow < TABLES_NUMBER
+    println("Доступность столиков на сегодня: $isAvailableToday\n" +
+            "Доступность столиков на завтра: $isAvailableTomorrow")
+}
+
+const val TABLES_NUMBER = 13
