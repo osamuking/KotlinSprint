98613a4b31c548edbb3221359f0f3f25919d0c00
osamuking <regpurp@outlook.com>
2023-04-05 21:33:14 +0900

    Completed task5 from lesson4

--- src/main/kotlin/Lesson4/lesson4_task5.kt
+++ src/main/kotlin/Lesson4/lesson4_task5.kt
@@ -1,2 +1,26 @@
 package Lesson4
 
+const val CREW_RECOMMEND_AMOUNT = 70
+const val CREW_MIN_AMOUNT = 55
+const val FOOD_MIN_AMOUNT = 50
+fun main(){
+
+    println("Для принятия решения о возможности отплытия ответьте на следующие вопросы:")
+    println("1. Есть ли повреждения корпуса?(true/false): ")
+    var isBodyDamaged = readln().toBoolean()
+    println("2. Численность экипажа (чел.): ")
+    var crewAmount = readln().toInt()
+    println("3. Количество провизии (ящиков): ")
+    var foodStock = readln().toInt()
+    println("4. Благоприятны ли метеоусловия?(true/false): ")
+    var isWeatherGood = readln().toBoolean()
+
+    var goodForSail = (!isBodyDamaged && (crewAmount > CREW_MIN_AMOUNT &&
+            crewAmount < CREW_RECOMMEND_AMOUNT) && foodStock > FOOD_MIN_AMOUNT) ||
+            (isBodyDamaged && crewAmount == CREW_RECOMMEND_AMOUNT && isWeatherGood
+             && foodStock > FOOD_MIN_AMOUNT)
+    if(goodForSail)
+        println("Отплытие разрешено")
+        else
+        println("Отплытие запрещено")
+}
