f718ff3a59fe5ecbb29feaa4b6a22794bc6ad9d1
osamuking <regpurp@outlook.com>
2023-04-06 08:35:44 +0900

    Completed task2 from lesson5

--- src/main/kotlin/Lesson5/lesson5_task1.kt
+++ src/main/kotlin/Lesson5/lesson5_task1.kt
@@ -1,2 +1,17 @@
 package Lesson5
 
+const val MAJORITY_AGE = 18
+fun main(){
+
+    val year = 2023
+
+    println("Введите свой год рождения:")
+    val yearOfBirth = readln().toInt()
+
+    if(year - yearOfBirth < MAJORITY_AGE){
+        println("Вернуться на главный экран")
+    } else {
+        println("Показать экран со скрытым контентом")
+    }
+
+}
