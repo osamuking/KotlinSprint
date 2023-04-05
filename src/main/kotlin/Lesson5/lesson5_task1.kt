9976f1bfcbc7d36af724087bdcab48f20379afb2
osamuking <regpurp@outlook.com>
2023-04-06 06:37:55 +0900

    Completed task1 from lesson5

--- src/main/kotlin/Lesson5/lesson5_task1.kt
+++ src/main/kotlin/Lesson5/lesson5_task1.kt
@@ -1,2 +1,13 @@
 package Lesson5
 
+fun main(){
+
+    print("\nПодтвердите, что вы человек, решите пример: 37 - 12 = ")
+    val input = readln().toInt()
+    val result = 25
+
+    if(input == result)
+        println("\nДобро пожаловать!")
+    else
+        println("\nДоступ запрещен")
+}
