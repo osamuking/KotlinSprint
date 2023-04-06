7fe07462faaa94b3f15c32038c39386e1018d763
osamuking <regpurp@outlook.com>
2023-04-06 09:20:30 +0900

    Completed the task in the wrong branch, so have to resolve that somehow

--- src/main/kotlin/Lesson5/lesson5_task1.kt
+++ src/main/kotlin/Lesson5/lesson5_task1.kt
@@ -1,17 +1,2 @@
 package Lesson5
 
-const val MAJORITY_AGE = 18
-fun main(){
-
-    val year = 2023
-
-    println("Введите свой год рождения:")
-    val yearOfBirth = readln().toInt()
-
-    if(year - yearOfBirth < MAJORITY_AGE){
-        println("Вернуться на главный экран")
-    } else {
-        println("Показать экран со скрытым контентом")
-    }
-
-}
