f8332c51ee3e8c29600fb4b21346439cffd4f03f
osamuking <regpurp@outlook.com>
2023-04-07 12:09:21 +0900

    Changed code in lesson4_task4

--- src/main/kotlin/Lesson4/lesson4_task4.kt
+++ src/main/kotlin/Lesson4/lesson4_task4.kt
@@ -1,25 +1,27 @@
 package Lesson4
 
-fun main(){
+fun main() {
 
-    println("Тренировки проводятся через день. Полный цикл охватывает 11 дней.")
-    print("Введите день (от 1 до 11): ")
+    print("Введите день: ")
 
     val day = readln().toInt()
-    val arms = day == 1 || day == 5 || day == 7
-    val legs = day == 3 || day == 7 || day ==11
-    val back = day == 3 || day == 5 || day ==9
-    val abs = day == 1 || day == 9 || day == 11
 
-    if(day%2 == 0) {
-        println("Сегодня день отдыха")
-    } else {
-        println(
-            """Упражнения для рук:    $arms
-        |Упражнения для ног:    $legs
-        |Упражнения для спины:  $back
-        |Упражнения для пресса: $abs
-    """.trimMargin()
-        )
+    /* val isEven = day%2 == 0
+    val arms = !isEven
+    val abs = !isEven
+    val legs = isEven
+    val back = isEven */
+
+    val arms = day % 4 == 1
+    val abs = day % 4 == 1
+    val legs = day % 4 == 3
+    val back = day % 4 == 3
+
+    if (!arms && !abs && !legs && !back) {
+        println("День отдыха")
+    } else if (arms && abs && !legs && !back) {
+        println("Упражнения для рук и пресса")
+    } else if (!arms && !abs && legs && back) {
+        println("Упражнения для ног и спины")
     }
 }
