933572854a61748d31f6ad41eb3f248213d93db1
osamuking <regpurp@outlook.com>
2023-04-08 15:22:04 +0900

    Completed task4 from lesson5

--- src/main/kotlin/Lesson5/lesson5_task4.kt
+++ src/main/kotlin/Lesson5/lesson5_task4.kt
@@ -1,2 +1,18 @@
 package Lesson5
+const val LOGIN = "Batman"
+const val PASSWORD = "password"
+fun main() {
 
+    println("Введите свой логин:")
+    val input1 = readln()
+    if (input1 == LOGIN) {
+        println("Введите пароль:")
+        val input2 = readln()
+        if (input2 == PASSWORD) {
+            println("Добро пожаловать!")
+        } else {
+            println("Неверный пароль")
+        }
+    } else {println("Пользователь с таким логином не обраружен.\n" +
+            "Зарегистрируйтесь на сайте.")}
+}
