41a5aa063cf71ee34089b1f5e4534013cf1b46a2
osamuking <regpurp@outlook.com>
2023-04-09 22:21:32 +0900

    Fixed a bug with the project structure

--- src/main/kotlin/Lesson5/lesson5_task5.kt
+++ src/main/kotlin/Lesson5/lesson5_task5.kt
@@ -1,14 +1,2 @@
 package Lesson5
 
-fun main(){
-
-    var greeting = "Good morning"
-    var userName = "dear customer"
-
-    println("$greeting, $userName!")
-
-    greeting = "Good evening"
-    userName = "valuable customer"
-
-    println("$greeting, $userName!")
-}
