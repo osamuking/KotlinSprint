f7699265a40ba42e5906ccda360103ef3875257e
osamuking <regpurp@outlook.com>
2023-04-13 08:50:15 +0900

    Completed task2 from lesson6

--- src/main/kotlin/Lesson6/lesson6_task2.kt
+++ src/main/kotlin/Lesson6/lesson6_task2.kt
@@ -1,2 +1,15 @@
 package Lesson6
 
+fun main(){
+
+    println("Задайте время в секундах:")
+    val time = readln().toInt()
+    var counter = time
+
+    while (counter > 0) {
+       // println(counter)
+        counter--
+        Thread.sleep(1000)
+    }
+    println("Прошло $time секунд")
+}
