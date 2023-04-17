86612fbcce7c80ad5043acf6438778a15437bed2
osamuking <regpurp@outlook.com>
2023-04-17 15:30:27 +0900

    Completed task1 from lesson7

--- src/main/kotlin/Lesson7/lesson7_task1.kt
+++ src/main/kotlin/Lesson7/lesson7_task1.kt
@@ -1,2 +1,13 @@
 package Lesson7
 
+fun main(){
+
+    println("Your new password:")
+
+    for (i in 0..2) {
+        val symbol1 = ('a'..'z').random()
+        val symbol2 = (0..9).random()
+        print(symbol1)
+        print(symbol2)
+    }
+}
