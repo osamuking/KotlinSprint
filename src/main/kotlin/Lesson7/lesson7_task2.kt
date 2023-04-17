a5a8223d6b4882930a77dc0bc56ea9feab75464d
osamuking <regpurp@outlook.com>
2023-04-17 16:54:25 +0900

    Completed task2 from lesson7

--- src/main/kotlin/Lesson7/lesson7_task2.kt
+++ src/main/kotlin/Lesson7/lesson7_task2.kt
@@ -1,2 +1,21 @@
 package Lesson7
 
+fun main() {
+
+    var code = (1000..9999).random()
+    println("Your authorization code: $code")
+    println("Enter your code:")
+    var codeInput = readln().toInt()
+
+
+    while (codeInput != code) {
+        Thread.sleep(2000)
+
+        code = (1000..9999).random()
+        println("Your authorization code: $code")
+        println("Enter your code again:")
+        codeInput = readln().toInt()
+
+    }
+    println("Welcome!")
+}
