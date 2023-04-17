3f5e9516e4cfdc0bd38fa1db43aa0d77845d09cd
osamuking <regpurp@outlook.com>
2023-04-17 20:31:29 +0900

    Completed task4 from lesson7

--- src/main/kotlin/Lesson7/lesson7_task4.kt
+++ src/main/kotlin/Lesson7/lesson7_task4.kt
@@ -1,2 +1,14 @@
 package Lesson7
 
+fun main() {
+    print("Enter the time: ")
+    val time = readln().toInt()
+
+    for (i in time-1 downTo 0) {
+        Thread.sleep(1000)
+        if (i == 0)
+            break
+        println(i)
+    }
+    print("Time is over")
+}
