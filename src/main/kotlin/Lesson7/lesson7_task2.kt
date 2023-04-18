6dd443778d4946f705aa1a4a30e761af1978cbfc
osamuking <regpurp@outlook.com>
2023-04-18 09:03:40 +0900

    Changed code

--- src/main/kotlin/Lesson7/lesson7_task2.kt
+++ src/main/kotlin/Lesson7/lesson7_task2.kt
@@ -2,20 +2,13 @@ package Lesson7
 
 fun main() {
 
-    var code = (1000..9999).random()
-    println("Your authorization code: $code")
-    println("Enter your code:")
-    var codeInput = readln().toInt()
-
-
-    while (codeInput != code) {
-        Thread.sleep(2000)
-
-        code = (1000..9999).random()
+    do {
+        var code = (1000..9999).random()
         println("Your authorization code: $code")
-        println("Enter your code again:")
-        codeInput = readln().toInt()
-
+        println("Enter your code:")
+        var codeInput = readln().toInt()
     }
+    while (codeInput != code)
+
     println("Welcome!")
 }
