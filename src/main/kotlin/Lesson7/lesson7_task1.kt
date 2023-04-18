fc71ef3b9d142321c2724f5dd043d2d6f92827dc
Sergei <77558887+osamuking@users.noreply.github.com>
2023-04-19 08:25:39 +0900

    Update lesson7_task1.kt
    Changed code

--- src/main/kotlin/Lesson7/lesson7_task1.kt
+++ src/main/kotlin/Lesson7/lesson7_task1.kt
@@ -1,24 +1,19 @@
 package Lesson7
 
 fun main(){
-    var symbol1 = '1'
-    var symbol2 = 0
-    var symbol3 = '1'
-    var symbol4 = 0
-    var symbol5 = '1'
-    var symbol6 = 0
-
+   
     println("Your new password:")
 
     for (i in 0..2) {
-        symbol1 = ('a'..'z').random()
-        symbol2 = (0..9).random()
-        symbol3 = ('a'..'z').random()
-        symbol4 = (0..9).random()
-        symbol5 = ('a'..'z').random()
-        symbol6 = (0..9).random()
+       var symbol1 = ('a'..'z').random()
+       var symbol2 = (0..9).random()
+       var symbol3 = ('a'..'z').random()
+       var symbol4 = (0..9).random()
+       var symbol5 = ('a'..'z').random()
+       var symbol6 = (0..9).random()
+       
+       val password = "$symbol1$symbol2$symbol3$symbol4$symbol5$symbol6"
+        println(password)
+        break
     }
-    val password = "$symbol1$symbol2$symbol3$symbol4$symbol5$symbol6"
-
-    println(password)
 }
