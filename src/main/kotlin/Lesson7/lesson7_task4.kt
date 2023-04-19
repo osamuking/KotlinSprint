17ab049761c71089734b47197fd69cb2d0be133a
osamuking <regpurp@outlook.com>
2023-04-19 10:09:00 +0900

    Brush up the code

--- src/main/kotlin/Lesson7/lesson7_task4.kt
+++ src/main/kotlin/Lesson7/lesson7_task4.kt
@@ -4,7 +4,7 @@ fun main() {
     print("Enter the time: ")
     val time = readln().toInt()
 
-    for (i in time-1 downTo 0) {
+    for (i in time - 1 downTo 0) {
         Thread.sleep(1000)
         /*if (i == 0)
             break*/
