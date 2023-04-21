c6b084993a91af37f7e21c00af3a2fea3656a395
osamuking <regpurp@outlook.com>
2023-04-21 15:29:46 +0900

    Completed task5 from lesson7

--- src/main/kotlin/Lesson7/lesson7_task5.kt
+++ src/main/kotlin/Lesson7/lesson7_task5.kt
@@ -1,2 +1,17 @@
 package Lesson7
 
+fun main(){
+    println("Задайте желаемую длину пароля:")
+    val passwordLength = readln().toInt()
+    var password = ""
+    print("Ваш пароль: ")
+
+    for (i in 1..passwordLength) {
+        val x = (1..9).random().toString()
+        val y = ('a'..'z').random()
+        val z = ('a'..'z').random().uppercaseChar()
+        val mergedString = x + y + z
+        password += mergedString.random()
+    }
+    println(password)
+}
