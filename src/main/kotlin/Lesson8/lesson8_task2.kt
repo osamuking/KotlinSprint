9326ed01ed6d533553840f905146f65258ca1705
osamuking <regpurp@outlook.com>
2023-04-22 21:29:04 +0900

    Completed task2 from lesson8

--- src/main/kotlin/Lesson8/lesson8_task2.kt
+++ src/main/kotlin/Lesson8/lesson8_task2.kt
@@ -1,2 +1,17 @@
 package Lesson8
 
+fun main() {
+    val salad = arrayOf("onion", "potato", "green peas", "egg", "sausage", "apple", "pickles", "mayonnaise")
+    println("Enter your inquiry:")
+    val ingredient = readln()
+
+    for (i in salad) {
+        if (ingredient in salad) {
+            println("Ingredient $ingredient was found in the recipe")
+            break
+        } else {
+            println("There is no such ingredient in the recipe")
+            break
+        }
+    }
+}
