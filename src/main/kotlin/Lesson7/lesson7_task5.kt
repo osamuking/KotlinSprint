4ac992ea6438d2ebbf1d179a127ebb5f4a6c3d18
osamuking <regpurp@outlook.com>
2023-04-23 22:19:20 +0900

    Changed code

--- src/main/kotlin/Lesson7/lesson7_task5.kt
+++ src/main/kotlin/Lesson7/lesson7_task5.kt
@@ -1,14 +1,18 @@
 package Lesson7
 
-fun main(){
+fun main() {
+    var password = ""
+    val x = 'a'..'z'
+    val y = 'A'..'Z'
+    val z = 1..9
+    val data = z + x + y
+
     println("Задайте желаемую длину пароля:")
     val passwordLength = readln().toInt()
-    var password = ""
     print("Ваш пароль: ")
+
     for (i in 1..passwordLength) {
-    var data = (('a'..'z') + ('A'..'Z') + (1..9)).random().toString()
-        password += data
+        password += data.random()
     }
     println(password)
-
 }
