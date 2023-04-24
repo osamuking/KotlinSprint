b8808c354a9299dffc19c8af4b6b8da48bdb1272
osamuking <regpurp@outlook.com>
2023-04-24 14:16:11 +0900

    Changed code

--- src/main/kotlin/Lesson8/lesson8_task1.kt
+++ src/main/kotlin/Lesson8/lesson8_task1.kt
@@ -15,52 +15,34 @@ package Lesson8
 fun main(){
 
     val weeklyViews = arrayOf(1, 22, 33, 44, 579, 1234, 5678)
+    val sum = 0
 
-    println(weeklyViews[0] + weeklyViews[1] + weeklyViews[2] + weeklyViews[3] + weeklyViews[4] + weeklyViews[5] +
-    weeklyViews[6])
+    for (i in weeklyView.indices)
+        sum += weeklyView[i]
+
+    println("\nКоличество просмотров за неделю составило $sum")
     }
     */
-//---------------------------------------------------------------------------------------
-// Или так:
-
-/* fun main() {
-    var mon = 0
-    var tue = 0
-    var wed = 0
-    var thu = 0
-    var fri = 0
-    var sat = 0
-    var sun = 0
-
-    val weeklyView = arrayOf(mon, tue, wed, thu, fri, sat, sun)
-
-    mon = 3
-    tue = 44
-    wed = 55
-    thu = 666
-    fri = 579
-    sat = 1234
-    sun = 5678
-
-
-    println("\nКоличество просмотров за неделю составило ${mon + tue + wed + thu + fri + sat + sun}")
-}
-*/
 //----------------------------------------------------------------------------------------
+
 // Или так:
 fun main() {
 
-    val weeklyView = arrayOf(0, 0, 0, 0, 0, 0, 0)
+    val weeklyView = Array(7){0}
+    var sum = 0
 
-    weeklyView[0] = 3
-    weeklyView[1] = 44
-    weeklyView[2] = 55
-    weeklyView[3] = 666
-    weeklyView[4] = 579
-    weeklyView[5] = 1234
-    weeklyView[6] = 5678
+    weeklyView[0] = 1
+    weeklyView[1] = 22
+    weeklyView[2] = 333
+    weeklyView[3] = 4444
+    weeklyView[4] = 55555
+    weeklyView[5] = 666666
+    weeklyView[6] = 7777777
 
+    for (i in weeklyView.indices)
+        sum += weeklyView[i]
 
-    println("\nКоличество просмотров за неделю составило ${weeklyView[0] + weeklyView[1] + weeklyView[3] +
-            weeklyView[4] + weeklyView[5] + weeklyView[6]}")
+    println("\nКоличество просмотров за неделю составило $sum")
+
 }
+
