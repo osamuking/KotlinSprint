05347a688d5f863399e99f46f593202599f6b157
osamuking <regpurp@outlook.com>
2023-05-01 21:00:51 +0900

    Complete task3 from lesson9

--- src/main/kotlin/Lesson9/lesson9_task3.kt
+++ src/main/kotlin/Lesson9/lesson9_task3.kt
@@ -1,2 +1,10 @@
 package Lesson9
 
+fun main(){
+    var omelet = mutableListOf<Int>(2, 50, 15)
+    println("For how many person would you like to make the dish?")
+    val numberOfPerson = readln().toInt()
+    val omelet2 = omelet.map { it * numberOfPerson }
+    println("For $numberOfPerson persons you need: Eggs - ${omelet2[0]}, Milk - ${omelet2[1]}, Butter -" +
+                    "${omelet2[2]}")
+}
