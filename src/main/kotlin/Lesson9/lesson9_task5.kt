7646f58ef139c252cfc928a77dba1af203dd812b
osamuking <regpurp@outlook.com>
2023-05-06 08:27:40 +0900

    Complete task5 from lesson9

--- src/main/kotlin/Lesson9/lesson9_task5.kt
+++ src/main/kotlin/Lesson9/lesson9_task5.kt
@@ -1,2 +1,46 @@
 package Lesson9
+/*
+Напиши программу, которая запрашивает у пользователя пять названий ингредиентов (каждый по отдельности).
 
+ – результат должен исключать повторы;
+ – список должен выводиться в алфавитном порядке;
+ – все элементы списка выведены в одну строку, разделяя слова запятыми;
+ – первое слово должно быть напечатано с заглавной буквы, в конце списка должна стоять точка.
+ */
+fun main(){
+
+    var i = 1
+    var number = ""
+    val listOfIngredients = mutableListOf<String>()
+    while (i <= 5) {
+        when (i) {
+            1 -> number = "first"
+            2 -> number = "second"
+            3 -> number = "third"
+            4 -> number = "fourth"
+            5 -> number = "fifth"
+        }
+        println("Enter the $number ingredient:")
+        val userInput = readln()
+        if (listOfIngredients.contains(userInput)) {
+            println("This ingredient is already in the list")
+            continue
+        }
+        listOfIngredients.add(userInput)
+            i++
+    }
+    listOfIngredients.sort()
+
+    val separator = ", "
+    val resultList = listOfIngredients.joinToString(separator)
+
+    print("${makeTheFirstLetterHuge(resultList)}.")
+}
+
+fun makeTheFirstLetterHuge(string: String): String {
+ return string.replaceFirstChar {
+     if (it.isLowerCase())
+         it.titlecase()
+     else it.toString()
+ }
+}
