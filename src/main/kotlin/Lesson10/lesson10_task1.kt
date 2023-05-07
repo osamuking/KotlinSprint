695519bdb26c38f0b7c3e29fbda303fc52e5c58f
osamuking <regpurp@outlook.com>
2023-05-07 21:16:21 +0900

    Completed task1 from lesson10

--- src/main/kotlin/Lesson10/lesson10_task1.kt
+++ src/main/kotlin/Lesson10/lesson10_task1.kt
@@ -1,2 +1,48 @@
 package Lesson10
 
+/*
+Напиши небольшую игру, в которой пользователь и компьютер по очереди
+бросают кости. Побеждает выбросивший наибольшее число.
+ – поочередно выводи сообщение о ходе игрока и компьютера (отображать
+ кто бросил, какие значения на кубиках);
+ – бросок костей вынеси в отдельную функцию, в которой генерируются
+ 2 случайных числа от 1 до 6;
+ – в отдельные переменные сохрани результат работы функции для игрока
+ и компьютера;
+ – после сравнения результатов, выведи соответствующее сообщение в консоль.
+ Например: "Победило человечество" или "Победила машина".
+ */
+fun main() {
+
+    print("Human's turn. Roll the dice!")
+    readln()
+    val human = humanRolls()
+    println("Homo Sapience has got $human ${grammar(human)}\n")
+    print("Super computer's turn. Roll the dice!")
+    readln()
+    val machine = machineRolls()
+    println("Super computer has got $machine ${grammar(machine)}\n")
+    if (human > machine)
+        println("The winner is Homo Sapience!")
+    else if (human < machine)
+
+        println("The winner is Super computer!")
+    else
+        println("We have a tie score.")
+
+}
+
+fun humanRolls(): Int {
+    return (1..6).random()
+}
+
+fun machineRolls(): Int {
+    return (1..6).random()
+}
+
+fun grammar(a: Int): String {
+    return if (a == 1)
+        "point"
+    else
+        "points"
+}
