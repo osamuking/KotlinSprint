8b05b6b1654b8349962a5b71b4e9f5eafa56a4b1
osamuking <regpurp@outlook.com>
2023-05-07 10:46:12 +0900

    Changed code

--- src/main/kotlin/Lesson9/lesson9_task5.kt
+++ src/main/kotlin/Lesson9/lesson9_task5.kt
@@ -1,17 +1,19 @@
 package Lesson9
 /*
-Напиши программу, которая запрашивает у пользователя пять названий ингредиентов (каждый по отдельности).
-
+Напиши программу, которая запрашивает у пользователя пять названий
+ ингредиентов (каждый по отдельности).
  – результат должен исключать повторы;
  – список должен выводиться в алфавитном порядке;
  – все элементы списка выведены в одну строку, разделяя слова запятыми;
- – первое слово должно быть напечатано с заглавной буквы, в конце списка должна стоять точка.
+ – первое слово должно быть напечатано с заглавной буквы, в конце списка
+ должна стоять точка.
  */
 fun main(){
 
-    var i = 1
     var number = ""
     val listOfIngredients = mutableListOf<String>()
+
+    /*var i = 1
     while (i <= 5) {
         when (i) {
             1 -> number = "first"
@@ -28,19 +30,35 @@ fun main(){
         }
         listOfIngredients.add(userInput)
             i++
+    }*/
+
+    for (i in 1 .. 5) {
+        when (i) {
+            1 -> number = "first"
+            2 -> number = "second"
+            3 -> number = "third"
+            4 -> number = "fourth"
+            5 -> number = "fifth"
+        }
+        println("Enter the $number ingredient:")
+        val userInput = readln()
+        if (listOfIngredients.contains(userInput)) {
+            println("This ingredient is already in the list")
+            continue
+        }
+        listOfIngredients.add(userInput)
     }
     listOfIngredients.sort()
 
     val separator = ", "
     val resultList = listOfIngredients.joinToString(separator)
 
-    print("${makeTheFirstLetterHuge(resultList)}.")
+    print("${capitalizeFirstLetter(resultList)}.")
 }
 
-fun makeTheFirstLetterHuge(string: String): String {
- return string.replaceFirstChar {
+    fun capitalizeFirstLetter(string: String) =
+  string.replaceFirstChar {
      if (it.isLowerCase())
          it.titlecase()
      else it.toString()
- }
 }
