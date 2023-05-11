a21e0c1c20cc5d01b0d692f43547596bbf5a4991
osamuking <regpurp@outlook.com>
2023-05-11 10:07:31 +0900

    Completed task2 from lesson10

--- src/main/kotlin/Lesson10/lesson10_task2.kt
+++ src/main/kotlin/Lesson10/lesson10_task2.kt
@@ -1,2 +1,37 @@
 package Lesson10
 
+/*
+Для регистрации в приложении пользователь придумывает логин и пароль.
+И пароль, и логин должны содержать в себе хотя бы 4 символа.
+Если всё верно, программа выводит приветственное сообщение.
+– логин и пароль должны считываться с консоли;
+– проверку длины логина или пароля вынеси в отдельную функцию, которая валидирует длину пароля;
+– если пароль меньше 4 символов – вывести сообщение: "Логин или пароль недостаточно длинные".
+ */
+
+fun main() {
+
+    println("Enter your login")
+    var log = readln()
+    while (checkLength(log) < 4) {
+        warning()
+        log = readln()
+    }
+
+    println("Enter your password")
+    var pass = readln()
+    while (checkLength(pass) < 4) {
+        warning()
+        pass = readln()
+
+    }
+    println("You have been registered")
+}
+
+fun checkLength(x: String): Int {
+    val y: List<Char> = x.toList()
+     return y.size
+}
+fun warning() {
+    println("You should use at least four symbols")
+}
