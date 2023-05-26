32ef248d39077bd7b6912126c1e5bd6780f9ca8c
osamuking <regpurp@outlook.com>
2023-05-26 15:32:37 +0900

    Completed task1 from lesson11

--- src/main/kotlin/OOP/Lesson11/lesson11_task1.kt
+++
@@ -1,2 +0,0 @@
-package OOP.Lesson11
-
---
+++ src/main/kotlin/OOP/Lesson11/task1/User.kt
@@ -0,0 +1,10 @@
+package OOP.Lesson11.task1
+
+class User(
+    val id: Int,
+    val login: String,
+    val password: String,
+    val email: String,
+) {
+
+}
